7ae8623cf7d5b0a29023d7e709b90927b326ece1
villageworks <villageworks@gmail.com>
2025-08-22 16:37:31 +0900

    MLS21: scott 課題2-1の実装（Serviceクラスの導入）

---
+++ src/app/master/StartUp.java
@@ -0,0 +1,9 @@
+package app.master;
+
+public class StartUp {
+
+	public static void main(String[] args) {
+		Menu.showMenu();
+	}
+
+}
---
+++ src/app/service/ProductRegistService.java
@@ -0,0 +1,14 @@
+package app.service;
+
+import app.common.Display;
+
+/**
+ * 商品追加サービス
+ */
+public class ProductRegistService {
+
+	public ProductRegistService() {
+		Display.showMessageln("商品追加");
+	}
+
+}
